fe0b839c9380559e3b4f393f1927d3895fcbd60a
hsc <hsc@hsc.hsc>
2013-09-08 13:47:58 +0200

    Added Mac OS X/iOS app entry point.

---
+++ main.m
@@ -0,0 +1,28 @@
+// Public Domain
+
+#import "framework/framework_api.h"
+
+#import <Foundation/NSAutoreleasePool.h>
+#import <Appkit/NSApplication.h>
+
+static NSAutoreleasePool *pool;
+
+static void
+cocoa_init(void)
+{
+    pool = [[NSAutoreleasePool alloc] init];
+    [NSApplication sharedApplication];
+}
+
+static void
+cocoa_finish(void)
+{
+    [pool drain];
+}
+
+int main(int argc, char* argv[])
+{
+    cocoa_init();
+    int result = RunFramework();
+    cocoa_finish();
+}
