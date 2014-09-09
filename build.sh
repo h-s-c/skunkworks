8b8356af8743fa694d6c48a07810c61c9c806108
hsc <hsc@lapbox>
2014-09-10 01:21:19 +0200

    Improved Android build.

--- build.sh
+++ build.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
-export SOURCE_DIR=$(pwd)
+export SRC_DIR=$(pwd)
+export BIN_DIR=$(pwd)/../binaries
 
 mkdir build 
 cd build
 
-mkdir linux-gcc-libstdc++
-cd linux-gcc-libstdc++
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=$SOURCE_DIR/../binaries/linux-gcc-libstdc++ -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=$SOURCE_DIR/../binaries/linux-gcc-libstdc++ -GNinja $SOURCE_DIR
+mkdir linux-host-gcc-libstdc++
+cd linux-host-gcc-libstdc++
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=$BIN_DIR/linux-host-gcc-libstdc++ -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=$BIN_DIR/linux-host-gcc-libstdc++ -GNinja $SRC_DIR
 ninja
 cd ..
 
-mkdir linux-clang-libstdc++
-cd linux-clang-libstdc++
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=$SOURCE_DIR/../binaries/linux-clang-libstdc++ -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=$SOURCE_DIR/../binaries/linux-clang-libstdc++ -GNinja $SOURCE_DIR
+mkdir linux-host-clang-libstdc++
+cd linux-host-clang-libstdc++
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=$BIN_DIR/linux-host-clang-libstdc++ -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=$BIN_DIR/linux-host-clang-libstdc++ -GNinja $SRC_DIR
 ninja
 cd ..
 
-mkdir android-gcc-libstdc++
-cd android-gcc-libstdc++
+mkdir android-armv7a-gcc-libstdc++
+cd android-armv7a-gcc-libstdc++
 unset ANDROID_NDK
-export ANDROID_STANDALONE_TOOLCHAIN=$SOURCE_DIR/toolchain
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=$SOURCE_DIR/cmake/ToolchainANDROID.cmake -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=$SOURCE_DIR/../binaries/android-gcc-libstdc++ -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=$SOURCE_DIR/../binaries/android-gcc-libstdc++ -GNinja $SOURCE_DIR
+export ANDROID_STANDALONE_TOOLCHAIN=$SRC_DIR/toolchain
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=$SRC_DIR/cmake/ToolchainANDROID.cmake -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=$(pwd)/libs/armeabi-v7a  -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=$(pwd)/libs/armeabi-v7a  -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY=$(pwd)/libs/armeabi-v7a -GNinja $SRC_DIR
 ninja
+cp $SRC_DIR/AndroidManifest.xml .
+android update project --name Skunkworks --path . --target "android-19"
+ant debug
+mkdir $BIN_DIR/android-armv7a-gcc-libstdc++
+cp $(pwd)/bin/Skunkworks-debug.apk $BIN_DIR/android-armv7a-gcc-libstdc++
 cd ..
 
 cd ..
