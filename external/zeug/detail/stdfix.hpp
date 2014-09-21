9b67c3fcca12e75b96eb28ff03f6bd3cd04567cd
hsc <hsc@lapbox>
2014-09-21 21:23:54 +0200

    Fixed Intel C++ build.

--- external/zeug/detail/stdfix.hpp
+++ external/zeug/detail/stdfix.hpp
@@ -16,9 +16,7 @@
 
 // C++11 workarounds 
 #if !defined(thread_local)
-#   if defined(COMPILER_ICC) && defined(COMPILER_HOST_GCC) && (COMPILER_VERSION_MAJOR<14)
-#       define thread_local __thread
-#   elif defined(COMPILER_MSVC) || (defined(COMPILER_ICC) && defined(COMPILER_HOST_MSVC))
+#if defined(COMPILER_MSVC) || (defined(COMPILER_ICC) && defined(COMPILER_HOST_MSVC))
 #       define thread_local __declspec(thread) 
 #   endif
 #endif
@@ -33,5 +31,36 @@ namespace std
     ss << from;
     return ss.str();
   }
+#endif
+
+#if defined(COMPILER_ICC) && defined(COMPILER_HOST_GCC) && (COMPILER_VERSION_MAJOR<16)
+    template<class T> struct _Unique_if {
+        typedef unique_ptr<T> _Single_object;
+    };
+
+    template<class T> struct _Unique_if<T[]> {
+        typedef unique_ptr<T[]> _Unknown_bound;
+    };
+
+    template<class T, size_t N> struct _Unique_if<T[N]> {
+        typedef void _Known_bound;
+    };
+
+    template<class T, class... Args>
+        typename _Unique_if<T>::_Single_object
+        make_unique(Args&&... args) {
+            return unique_ptr<T>(new T(std::forward<Args>(args)...));
+        }
+
+    template<class T>
+        typename _Unique_if<T>::_Unknown_bound
+        make_unique(size_t n) {
+            typedef typename remove_extent<T>::type U;
+            return unique_ptr<T>(new U[n]());
+        }
+
+    template<class T, class... Args>
+        typename _Unique_if<T>::_Known_bound
+        make_unique(Args&&...) = delete;
 #endif
 }
--- external/zeug/string_hash.hpp
+++ external/zeug/string_hash.hpp
@@ -16,20 +16,14 @@ class string_hash
 { 
   public:
 
-    explicit constexpr string_hash(const char* str):
-    hash(hashString(str))
-    {
-    }
-    
-    explicit constexpr string_hash(const std::int64_t hash):
-    hash(hash)
-    {
-    }
-    
-    explicit constexpr string_hash(void* hash):
-    hash(*(std::int64_t*)(hash))
-    {
-    }
+    explicit constexpr string_hash(const char* str) : hash(hashString(str)) {}
+    explicit constexpr string_hash(const std::int64_t hash) : hash(hash) {}
+    #if defined(COMPILER_ICC) && defined(COMPILER_HOST_GCC) && (COMPILER_VERSION_MAJOR<16)    
+    //non-constant initializer for constexpr constructor
+    #else
+    explicit constexpr
+    #endif
+    string_hash(void* hash) : hash(*(std::int64_t*)(hash)){}
     
     inline size_t Size() const
     {
@@ -55,16 +49,6 @@ class string_hash
     {
         return this->hash != other.hash;
     }
-    
-    /*inline bool operator==(const std::int64_t& other) const
-    {
-        return this->hash == other;
-    }
-    
-    inline bool operator==(void* other) const
-    {
-        return this->hash == *reinterpret_cast<std::int64_t*>(other);
-    }*/
     
   private:        
     /* Performs a compile time recursive string hash using the djb2 algorithm explained here: http://www.cse.yorku.ca/~oz/hash.html*/
